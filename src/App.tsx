eba7fcc7e1637e6d78978b8bfeb4263e8e677da1
Dinesh Poudel <diensh2019@cdcsit.edu.np>
2020-12-26 10:36:53 +0545

    changes for theming

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navigate from './contents/Navigation';
 import Contents from './contents/Contents';
 import { Grid } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
 
 const App: FunctionComponent<{}> = () => {
   const [theme, setTheme] = useState("dark")
